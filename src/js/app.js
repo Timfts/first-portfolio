265a78c874bf50eaee7873b4abcef1ece9db08fb
italo fontes <italo5123@hotmail.com>
2018-10-04 17:36:50 -0300

    book animation added

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,56 @@
-const test = batata => {
-    console.log(batata);
+//Helpers
+
+function $(query){
+    return document.querySelector(query);
 }
 
-test('hahha');
+function debounce(func, wait = 20, immediate = true){
+    let timeout;
+    return function() {
+        let context = this,
+            args = arguments;
+
+        function later(){
+            timeout = null;
+            if(!immediate) func.apply(context, args);
+        }
+
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow) func.apply(context, args);
+
+    }
+}
+
+//Elements
+
+const elements = {
+    book : $('.book')
+};
+
+
+//Scroll animations
+
+const posTriggers = {
+    book: elements.book.parentElement.offsetTop,
+    bookParentSize: elements.book.parentElement.offsetHeight
+}
+
+function checkSlide(e){
+    const halfScreen = window.scrollY + (window.innerHeight / 2);
+
+    if(screen.width >= 600){
+
+        if(halfScreen > posTriggers.book) elements.book.classList.add('book--active');
+        else elements.book.classList.remove('book--active');
+
+    }
+    
+
+
+   
+
+
+}
+window.addEventListener('scroll', debounce(checkSlide));
